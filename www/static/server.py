be8140f4b02d320e091411b8d8739488190a02b5
Howard <martin119@126.com>
2015-10-14 11:11:48 +0800

    modify a file

--- www/static/server.py
+++ www/static/server.py
@@ -11,7 +11,7 @@ def application(environ, start_response):
 
 
 # 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
-httpd = make_server('', 8001, application)
-print('Serving HTTP on port 8001...')
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000...')
 # 开始监听HTTP请求:
 httpd.serve_forever()
